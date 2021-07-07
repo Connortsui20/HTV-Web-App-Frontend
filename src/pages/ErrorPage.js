5710dd5360d8c8a637c854f8870454af0de6cfa0
Connor Tsui <connor.tsui20@gmail.com>
2021-07-07 13:55:46 +0800

    day 4 version 2.2: more better

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,15 +1,15 @@
-import WrongRedemptionEn from '../images/img_wrong_redemption@3x.png'
+import WrongRedemptionCn from '../images/img_wrong_redemption_cn@3x.png'
+import WrongRedemptionEn from '../images/img_wrong_redemption_en@3x.png'
 
 
 export default function ErrorPage({language}) {
 
-    //if specific language show different picture
     let errorPicture = "";
 
     if (language === "en") {
         errorPicture = WrongRedemptionEn;
     } else if (language === "cn") {
-        errorPicture = "unknown";
+        errorPicture = WrongRedemptionCn;
     }
 
     return (
